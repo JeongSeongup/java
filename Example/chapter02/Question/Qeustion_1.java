305d177cc55e240ff69cb3a060c6aa09901a6f00
JSE <wjdtjddjq123@gmail.com>
2023-03-03 16:06:38 +0900

    Create Qeustion_1.java
    더 알아보기
    
    서로 다른 정수 자료형인 Byte와 Short형 변수의 값을 더하여 결과 값을 출력

---
+++ Example/chapter02/Question/Qeustion_1.java
@@ -0,0 +1,10 @@
+package chapter02.Question;
+// 더 알아보기 
+// 서로 다른 정수 자료형인 Byte와 Short형 변수의 값을 더하여 결과 값을 출력
+public class Qeustion_1 {
+    public static void main(String[] args){
+      short sVal = 10;
+      byte bVal = 20;
+      System.out.println(sVal+bVal);
+    } 
+}
