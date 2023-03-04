93b2cdc1e8b3f87cab1c483a274579bd2c69a9d4
JSE <wjdtjddjq123@gmail.com>
2023-03-04 17:45:05 +0900

    상수사용하기
    상수 선언 후 사용

---
+++ Example/chapter02/Constans.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class Constans {
+    public static void main(String[] args){
+        final int Max_Num = 100;
+        final int Min_num ;
+        
+        Min_num = 0;
+
+        System.out.println(Max_Num);
+        System.out.println(Min_num);
+    }
+}
