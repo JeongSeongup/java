f8122c19932407cb755438126917bae518240737
JSE <wjdtjddjq123@gmail.com>
2023-03-06 16:03:48 +0900

    Breake문
    Breake문

---
+++ Example/chapter04/lean/lean2.java
@@ -0,0 +1,14 @@
+package chapter04.lean;
+// 구구단을 짝수 단만 출력하도록 프로그램을 만들어보세요
+public class lean2 {
+    public static void main(String[] args){
+        int dan;
+        int items;
+
+        for( dan=1; dan <=9; dan++ ){
+            System.out.println(dan);
+            for( items=1; items <=9; items++ );
+            System.out.println(items);
+            }
+        }
+}
