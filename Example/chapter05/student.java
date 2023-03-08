0d60b392d3bd547668adc99ff103fb467c529b3f
JSE <wjdtjddjq123@gmail.com>
2023-03-08 13:57:45 +0900

    java Class
    23.03.08

---
+++ Example/chapter05/student.java
@@ -0,0 +1,13 @@
+package chapter05;
+
+public class student {
+    int studentID;
+    String studentName;
+    int grade;
+    String address;
+
+    public void ShowStudentInfo(){
+        System.out.println(studentName+","+address);
+    }
+
+}
