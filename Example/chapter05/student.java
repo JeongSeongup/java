d18572eed50c78329a00ecf60f7f76529fa9cb86
JSE <wjdtjddjq123@gmail.com>
2023-03-08 14:21:18 +0900

    Student Test
    Student Class Name Edit

--- Example/chapter05/student.java
+++ Example/chapter05/student.java
@@ -1,17 +1,31 @@
 package chapter05;
 
-public class student {
+public class Student {
     int studentID;
     String studentName;
     int grade;
     String address;
 
-    public void ShowStudentInfo(){
-        System.out.println(studentName+","+address);
-    }
+    // public void ShowStudentInfo(){
+    //     System.out.println(studentName+","+address);
+    // }
 
     public String getStudentName(){
         return studentName;
+        // 학생이름을 반화하는 메서드
+    }
+
+    public void setStudentName(String name){
+        studentName = name;
+        // 학생이름을 부여하는 메서드
+    }
+
+    public static void main(String[] args){
+        Student studentAhn = new Student(); //Student Class 생성
+        studentAhn.studentName = "안연수";
+
+        System.out.println(studentAhn.studentName);
+        System.out.println(studentAhn.getStudentName());
     }
 
 }
