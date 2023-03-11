2bc82aacb3cd2c2bf47bc3dc58b0f2ac141c9420
JSE <wjdtjddjq123@gmail.com>
2023-03-11 09:18:50 +0900

    Speling Edit

--- Example/chapter05/PersonTest.java
+++ Example/chapter05/PersonTest.java
@@ -2,6 +2,6 @@ package chapter05;
 
 public class PersonTest{
     public static void main(String[] args){
-        Person personlee = new person()
+        Person personlee = new Person();
     }
 }
